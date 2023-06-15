f578e0f13481d882229c7f713248705d822200eb
will henrique <willhc1984@gmail.com>
2023-06-15 14:31:44 -0300

    Person - save

--- src/main/java/com/cursospring/api/PersonController.java
+++ src/main/java/com/cursospring/api/PersonController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,7 +27,7 @@ public class PersonController {
 	}
 	
 	@PostMapping
-	public ResponseEntity<Object> save(Person person){
+	public ResponseEntity<Object> save(@RequestBody Person person){
 		return ResponseEntity.status(HttpStatus.CREATED).body(personService.save(person));
 	}
 
